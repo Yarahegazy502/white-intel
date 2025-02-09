02cb116c817334d3639c6c190e353b8748cfecd5
yarahegazy502 <yarahegazy502@gmail.com>
2025-02-09 23:46:32 +0200

    fix

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AlertsService } from './../../services/alerts.service';
 import { FormBuilder, ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser'; // For SEO purposes
@@ -12,11 +13,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   loginForm = this.fb.group({
-    email: ['daxd404@gmail.com', [Validators.required]],
-    password: ['0Xdax404#', [Validators.required]]
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]]
   })
 
-  constructor(private fb: FormBuilder, private router: Router, private title: Title// For SEO
+  constructor(private fb: FormBuilder, private alertsService: AlertsService, private router: Router, private title: Title// For SEO
   ) { }
 
   ngOnInit(): void {
@@ -25,9 +26,15 @@ export class LoginComponent {
 
   login() {
     let form = this.loginForm.value;
-    if (form.email == 'daxd404@gmail.com' && form.password == '0Xdax404#') {
-      localStorage.setItem('token', 'mklmknjlknknkl');
-      this.router.navigate(['/'])
+    if (this.loginForm.valid) {
+      if (form.email == 'daxd404@gmail.com' && form.password == '0Xdax404#') {
+        localStorage.setItem('token', 'mklmknjlknknkl');
+        this.router.navigate(['/'])
+      } else {
+        this.alertsService.openToast('error', 'Error', 'Email or Password not correct')
+      }
+    } else {
+      this.alertsService.openToast('error', 'Error', 'Email and Password is Required')
     }
   }
 
